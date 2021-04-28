23c89bf4c27746b2e3c69d68ff6abb2d9157d65b
mflorezr <mflorezr@eafit.edu.co>
2021-04-28 01:06:08 -0500

    Plot front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import PlotForm from "./Components/PlotForm";
+import {BrowserRouter, HashRouter, Link, Route} from 'react-router-dom'
+import PlotList from "./Components/PlotList";
+import {Nav, Navbar} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Navbar bg="primary" variant="dark">
+              <Navbar.Brand href="#home">Evergreen Production</Navbar.Brand>
+              <Nav className="mr-auto">
+                  <Nav.Link as={Link} to="/createPlot">Create Plot
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/plotList">Plot List</Nav.Link>
+              </Nav>
+          </Navbar>
+          <Route path='/createPlot' component = {PlotForm}/>
+          <Route path='/plotList' component = {PlotList}/>
+      </BrowserRouter>
+
   );
 }
 
